2ffa7b42d369bbe6fc9ae0307da01c96cca7c0b2
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-09-28 20:16:20 +0000

    Tidy misc formatting errors

--- run.py
+++ run.py
@@ -3,37 +3,62 @@ class Player:
     def __init__(self):
         self.name = ''
         self.status = []
-you = player()
+        self.room = ["porch"]
+
+
+you = Player()
+
 
 # Title screen 
 def title_screen_selections():
     option = input(">")
-    if option.lower()=="start":
+    if option.lower() == "start":
         start_game()
-    elif option.lower()=="character":
+    elif option.lower() == "character":
         character_index()
-    elif option.lower()=="quit":
-        sys.exit()
     while option.lower() not in ["start", "character", "quit"]:
         print("please enter a valid option")
         option = input(">")
-        if option.lower()=="start":
+        if option.lower() == "start":
             start_game()
-        elif option.lower()=="character":
+        elif option.lower() == "character":
             character_index()
-        elif option.lower()=="quit":
+        elif option.lower() == "quit":
             sys.exit()
-    
+
+
 def title_screen():
     os.system("clear")
     print("################")
     print("# Closing Time #")
     print("################")
     print("#     PLAY     #")
-    title_screen_selections()]
+    title_screen_selections()
+
 
 def character_index():
     print("Placeholder charachter bio")
     print("Placeholder charachter bio")
     print("Placeholder charachter bio")
     print("Placeholder charachter bio")
+
+
+# Logic
+def start_game():
+    return
+
+
+NARRATIVE = "narrative"
+
+
+player_state = {"meanie": False,
+                "stinky": False,
+                "cursed": False
+                }
+
+# Story/Narrative
+def room():
+    """
+    Prints the room the player is in (Indicates branch)
+    """
+    print("\n" + you.room.capitalize())
