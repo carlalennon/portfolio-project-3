2d2fb153b940271bed38e05dd2581c2dc5167c43
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-09-28 19:52:11 +0000

    Title screen options

--- run.py
+++ run.py
@@ -3,3 +3,24 @@ class Player:
     def __init__(self):
         self.name = ''
         self.status = []
+you = player()
+
+# Title screen 
+def title_screen_selections():
+    option = input(">")
+    if option.lower()=="start":
+        start_game()
+    elif option.lower()=="character":
+        character_index()
+    elif option.lower()=="quit":
+        sys.exit()
+    while option.lower() not in ["start", "character", "quit"]:
+        print("please enter a valid option")
+        option = input(">")
+        if option.lower()=="start":
+            start_game()
+        elif option.lower()=="character":s
+            character_index()
+        elif option.lower()=="quit":
+            sys.exit()
+    
