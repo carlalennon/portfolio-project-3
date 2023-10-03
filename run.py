aba7cce5bd0e7d424c35151b8e8cea05ea13dc4e
carlalennon <99259113+carlalennon@users.noreply.github.com>
2023-10-03 20:23:39 +0000

    Populate dicitonary

--- run.py
+++ run.py
@@ -14,6 +14,7 @@ class Player:
 you = Player()
 
 
+
 def title_screen():
     print("################")
     print("# Closing Time #")
@@ -186,6 +187,8 @@ def player_input_branch_definition():
     if action.lower() == "gamble":
         branch_fruit_machine_init()
         Player.branch = 1
+    elif action.lower() == "pool table":
+        Player.stage == 20
     else:
         print("Please enter a valid option 2")
         action = input("> .. ")
@@ -249,27 +252,28 @@ debug_dict = {
     },
     3: {
         "Win": {
-                "Choice": "Challenge him",
+                "Choice": "stay",
                 "Win": True,
                 "Flavour": {
-                    0: "Mairead's son is drunk",
-                    1: "The game is an easy win for you",
-                    2: "You shake hands and part ways as equals"
+                    0: "As you finish your drink you feel a little",
+                    1: "better. With a sense of comfort, you survey",
+                    2: "Your surroundings."
                 },
-                "Score": 250
+                "Score": 250,
+                "narrative": 4
                 },
         "Lose": {
-                "Choice": "Don't challenge him",
+                "Choice": "hide",
                 "Win": False,
                 "Flavour": {
-                    0: "He calls you a wimp",
-                    1: "He assures you he will meet you outside later",
-                    2: "You'd better not leave early tonight..."
+                    0: "As you hurry toward the facilities, you step",
+                    1: "into a puddle and sprawl to the floor. You are",
+                    2: "all sticky and dirty now."
                 },
                 "Score": -250,
-                "Modifier": "Enemy: You will be beaten if leaving before closing time"
-                }
-        },
+                "narrative": 9
+            },
+    },
     4: {
         "Win": {
             "Choice": "pool",
@@ -287,7 +291,7 @@ debug_dict = {
             "Win": False,
             "Flavour": {
                 0: "He doesn't want to talk about it, what's",
-                1: "wrong with you? \n",
+                1: "wrong with you?",
                 2: "Maybe a game of pool will cheer him up."  
             },
             "Score": -100,
@@ -331,7 +335,149 @@ debug_dict = {
             "Score": 250,
             "narrative": 5
         }
-    }
+    },
+    8: {
+        0: "Game over condition"
+    },
+    9: {
+        "Win": {
+            "Choice": "birthday party",
+            "Win": True,
+            "Flavour": {
+                0: "You hide in the back of the group of women,",
+                1: "hoping they don't notice you."
+            },
+            "Score": 0,
+            "narrative": 10
+        },
+        "Lose": {
+            "Choice": "tomas",
+            "Win": False,
+            "Flavour": {
+                0: "You shuffle toward Tomas, hoping that he",
+                1: "will tolerate your company."
+            },
+            "Score": 0,
+            "narrative": 13
+        }
+    },
+    10: {
+        "Win": {
+            "Choice": "tell",
+            "Win": True,
+            "Flavour": {
+                0: "You explain what happened to you, that you",
+                1: "fell in a puddle and came here to hide so you",
+                2: "wouldn't be alone.",
+                3: "The group 'awwww's and hands you a gin and tonic."
+            },
+            "Score": 250,
+            "narrative": 11
+        },
+        "Lose": {
+            "Choice": "blame",
+            "Win": False,
+            "Flavour": {
+                0: "You decide to blame someone. You accuse",
+                1: "one of the women. She starts to cry. The other",
+                2: "women are not fooled, and ask you to leave their",
+                3: "table."
+            },
+            "Score": -300,
+            "narrative": 14
+        }
+    },
+    11: {
+        "Win": {
+            "Choice": "join",
+            "Win": True,
+            "Flavour": {
+                0: "You get up and join them on the dance floor."
+            },
+            "Score": 300,
+            "narrative": 12
+        },
+        "Lose": {
+            "Choice": "not",
+            "Win": False,
+            "Flavour": {
+                1: "You sit awkwardly among the party decorations",
+                2: "as the women get up and dance."
+            },
+            "Score": -200,
+            "narrative": 19
+        }
+    },
+    12: "Game over condition",
+    13: {
+        "Win": {
+            "Choice": "sympathise",
+            "Win": True,
+            "Flavour": {
+                0: "You nod your head at this rant.",
+                1: "This makes Tomas very happy! He buys you a",
+                2: "drink and launches into another speech..."
+            },
+            "Score": 100,
+            "narrative": 15
+        },
+        "Lose": {
+            "Choice": "ignore",
+            "Win": False,
+            "Flavour": {
+                0: "You stare at Tomas, stone-faced.",
+                1: "He is not happy with you.",
+                2: "'I'm not making small talk with the type",
+                3: "of eejit who falls into puddles!'",
+                4: "He turns his back to you."
+            },
+            "Score": -100,
+            "narrative": 18
+        }
+    },
+    14: "Game over condition",
+    15: {
+        "Win": {
+            "Choice": "listen",
+            "Win": True,
+            "Flavour": {
+                0: "Tomas's next story is about aliens coming to",
+                1: "his farm and shooting at his cows.",
+                2: "It's actually pretty funny!"
+            },
+            "Score": 250,
+            "narrative": 16
+        }
+    },
+    16: {
+        "Win": {
+            "Choice": "nothing",
+            "Win": True,
+            "Flavour": {
+                0: "Tomas, ever the picture of manly stoicism,",
+                1: "accepts your silence as a gesture of manly",
+                2: "comradery."
+            },
+            "Score": 100,
+            "narrative": 17
+        },
+        "Lose": {
+            "Choice": "tell",
+            "Win": False,
+            "Flavour": {
+                0: "Tomas looks hurt, he wasn't trying to be",
+                1: "funny. He is no longer interested in",
+                2: "talking to you."
+            },
+            "Score": -100,
+            "narrative": 18
+        }
+    },
+    17: "Game over condition",
+    18: "Game over condition",
+    19: "Game over condition",
+
+        
 }
 
 
@@ -340,7 +486,7 @@ narrative_dict = {
         0: {
              0: "You head down to your local on a Friday night,",
              1: "there's a loud buzz of conversation and music playing.",
-             2: "The air smells like wood and alchohol. ",
+             2: "The air smells like wood and alcohol. ",
              3: "You could go to the fruit machine to [Gamble]",
              4: "or try your luck at the [Pool table].",
              5: "The owner [Sean] is watching a game in the corner.",
@@ -384,7 +530,7 @@ narrative_dict = {
             2: "the night as new friends."
         },
         7: {
-            0:"Your pride stinging, you look around for a",
+            0: "Your pride stinging, you look around for a",
             1: "peaceful resolution. Your eyes fall on the pool",
             2: "table. Will you [Challenge] him to a game or",
             3: "[Ignore] him?"
@@ -392,80 +538,81 @@ narrative_dict = {
         8: {
             0: "You have been thrown out of the pub.",
             1: "Game over. Try again?"
+        },
+        9: {
+            0: "You make a break for the facilities to hide.",
+            1: "In your haste, you slip and fall into a puddle.",
+            2: "You are sticky and dirty, you smell foul. Do you",
+            3: "take shelter in the [Birthday Party] or do you",
+            4: "go and talk to [Tomas]?"
+        },
+        10: {
+            0: "The women are having a great time at their party, they",
+            1: "hardly notice as you place your drink down between pitchers",
+            2: "of gin on the table.",
+            3: "Suddenly, the one wearing a sash wrinkles her nose. She",
+            4: "turns to the others and asks 'What's that smell?'",
+            5: "Do you [Tell] them about the puddle, or [Blame] someone",
+            6: "else?"
+        },
+        11: {
+            0: "Nestled in the gang with a drink in your hand, you feel",
+            1: "as though your night may finally be looking up. A song",
+            2: "starts to blare, and the girls get up to dance. Do you",
+            3: "[Join] them or [Not]?"
+        },
+        12: {
+            0: "You dance the night away with your new pals. All grievances",
+            1: "From earlier in the night are forgotten.",
+            2: "Before you know it, it's closing time. You walk home with",
+            3: "aching feet and a smile on your face."
+        },
+        13: {
+            0: "Tomas give you a wide grin.",
+            1: "Hello, how are you?' he asks. Before",
+            2: "you can answer, he launches into a long and terrible",
+            3: "story about his recent health issues, the cost of petrol,",
+            4: "and how immigrants are ruining everything.",
+            5: "He finishes and waits for your reply. Do you [Sympathise]",
+            6: "or [Ignore] him?"
+        },
+        14: {
+            0: "After the loud sobs of the birthday girl, no one",
+            1: "else in the pub is willing to talk to you.",
+            2: "You leave early, hanging your head in shame."
+        },
+        15: {
+            0: "Tomas rattles on about how the youth of today are",
+            1: "hopeless, and how telly isn't as good as it used to",
+            2: "be. How much more of this can you endure?",
+            3: "Can you [Listen] to some more, or do you [Stop] him?"
+        },
+        16: {
+            0: "With a smile on your face, you thank Tomas for the",
+            1: "story. You never knew he was so funny!",
+            2: "He asks what you're smiling about. Do you [Tell] him",
+            3: "he's secretly funny, or do you say [Nothing]?"
+        },
+        17: {
+            0: "Tomas continues to chatter happily and buys you some",
+            1: "drink to get you to stick around.",
+            2: "You happily accept, and spent the night listening to",
+            3: "his stories. The night flies by, and all of a sudden",
+            4: "it's closing time. You walk into the night having made",
+            5: "a new friend."
+        },
+        18: {
+            0: "When even the chattiest person in the pub won't talk",
+            1: "to you, it's time to head home."
+        },
+        19: {
+            0: "Alone, smelling like floor water, and all the gin",
+            1: "gone, it's time to head home in shame.",
+            2: "Maybe you'll have a better time another night."
         }
+        
 }
-# Player branch nested dictionaries
-old = {
-    5: {
-        '''"Choice": "Challenge him",
-        "Win": True,
-        "Flavour": {
-            0: "Mairead's son is drunk",
-            1: "The game is an easy win for you",
-            2: "You shake hands and part ways as equals"
-        },
-        "Score": 250'''
-    },
-    6: {
-        '''"Choice": "Don't challenge him",
-        "Win": False,
-        "Flavour": {
-            0: "He calls you a wimp",
-            1: "He assures you he will meet you outside later",
-            2: "You'd better not leave early tonight..."
-        },
-        "Score": -250,
-        "Modifier": "Enemy: You will be beaten if leaving before closing time"'''
-    },
-    7: {
-        "Choice": "Beat Sean at pool",
-        "Win": False,
-        "Flavour": {
-            0: "Sean is annoyed at your insolence",
-            1: "He throws you out of his bar",
-            2: "Mairead's son beats you as he turns a blind eye"
-        },
-        "Score": -250,
-        "Modifer": "This is a game over."
-    },
-    8: {
-        "Choice": "Let Sean win at pool",
-        "Win": True,
-        "Flavour": {
-            0: "Sean is glad of the distraction",
-            1: "He feels like a winner!",
-            2: "He gives you a drink -- on the house"
-        },
-        "Score": 250
-    },
-    9: {
-        "Win": True,
-        "Flavour": {
-            0: "It's closing time already, your night is over",
-            1: "Mairead's son is nowhere to be seen as you leave the bar",
-            2: "You walk into the night with Sean as a new friend"
-        }
-    },
-    10: {
-        "Choice": "Hide in the bathroom",
-        "Win": False,
-        "Flavour": {
-            0: "On the way to the loo, you slip in a puddle",
-            1: "You're sticky and dirty now",
-            2: "Your pride stinging, you head to the bar"
-        },
-        "Score": -100
-    },
-    11: {
-        "Choice": "Ask Sean what's wrong",
-        "Win": False,
-        "Flavour": {
-            0: "He obviously doesn't want to talk about it",
-            1: "What's wrong with you?"
-        },
-        "Score": -100
-    }
-}
+
 
 
 # Game calls
